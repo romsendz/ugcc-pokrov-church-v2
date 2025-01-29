4b1dae273752c72c350a89b37a6943a2567c2297
romsendz <sendzyuk.r@gmail.com>
2025-01-29 21:13:42 +0100

    logo

--- src/layouts/Header/index.tsx
+++ src/layouts/Header/index.tsx
@@ -2,6 +2,13 @@ import MobileNav from "./MobileNav";
 const Header = () => {
   return (
     <>
+        <Link href="/" className="flex gap-1">
+          <Image src={"/svgs/logo.svg"} alt="logo" width={60} height={60} />
+          <h5 className="whitespace-nowrap text-brand">
+            Парафія Покрови Пресвятої <br /> Богородиці{" "}
+            <span className="text-base text-white">м. Заліщики</span>
+          </h5>
+        </Link>
         <MobileNav />
     </>
   );
