1abe25ed1c29e2669d35ee8dd5fe38bc78ba9567
romsendz <sendzyuk.r@gmail.com>
2025-01-29 21:14:44 +0100

    dymamic and responsive header

--- src/layouts/Header/index.tsx
+++ src/layouts/Header/index.tsx
@@ -1,7 +1,25 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import clsx from "clsx";
+import { usePathname } from "next/navigation";
 import MobileNav from "./MobileNav";
 const Header = () => {
+  const pathname = usePathname();
+  const isHomePage = pathname === "/";
   return (
     <>
+      <header
+        className={clsx(
+          "relative flex max-h-[var(--header-height)] justify-between gap-2 bg-header p-2 text-brand lg:max-h-[unset] lg:gap-5 lg:border-b-[1px_solid_#e29579] lg:py-4",
+          {
+            "lg:absolute lg:top-0 lg:z-[2] lg:w-full lg:bg-opacity-40 lg:bg-[linear-gradient(180deg,_rgba(0,_0,_0,_0.7),_rgba(0,_0,_0,_0))] lg:backdrop-blur-sm":
+              isHomePage,
+            "lg:bg-header": !isHomePage,
+          },
+        )}
+      >
         <Link href="/" className="flex gap-1">
           <Image src={"/svgs/logo.svg"} alt="logo" width={60} height={60} />
           <h5 className="whitespace-nowrap text-brand">
@@ -10,6 +28,7 @@ const Header = () => {
           </h5>
         </Link>
         <MobileNav />
+      </header>
     </>
   );
 };
