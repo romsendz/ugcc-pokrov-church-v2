579b8c09854adb98d04165e1fe991e353a7bdfd6
romsendz <sendzyuk.r@gmail.com>
2025-01-29 21:24:16 +0100

    added quick access butoon to index (home) page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import HeroVideo from "@components/HeroVideo";
+import Link from "next/link";
 import LiveStreamButton from "@components/LiveStreamButton";
 import Image from "next/image";
 
@@ -6,73 +7,45 @@ export default function Home() {
   return (
     <div className="flex flex-col lg:relative">
       <HeroVideo />
-            <LiveStreamButton noLink={true} fallbackIconSize={25} />
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+      <hr className="mt-10 h-2 w-full border-0 bg-[#a19d99] lg:hidden" />
+      <section className="flex flex-col lg:absolute lg:top-[20%] lg:z-[1] lg:rounded-br-xl lg:rounded-tr-xl lg:bg-[#00000050] lg:backdrop-blur-md lg:backdrop-filter">
+        <div className="m-0 mr-[unset] flex w-full pl-0 lg:grid lg:grid-rows-[1fr_min-content_1fr]">
+          <Link
+            href={"/live-stream"}
+            className="flex flex-1 cursor-pointer flex-col items-center justify-between gap-4 bg-[#e6ded6] p-3 transition-colors hover:bg-[#e2c8ae] lg:flex-row lg:rounded-tr-xl lg:border-2 lg:border-l-[0] lg:border-transparent lg:border-b-transparent lg:bg-transparent lg:hover:border-[#e29579] lg:hover:bg-transparent"
           >
-            Read our docs
-          </a>
+            <span className="text-center text-2xl font-bold lg:text-brand">
+              Пряма <br className="sm:hidden" /> трансляція
+            </span>
+            <LiveStreamButton noLink={true} fallbackIconSize={25} />
+          </Link>
+          <div className="m-0 w-2 bg-[#a19d99] lg:hidden" />
+          <hr className="border-1 hidden border-[#e29579] lg:block" />
+          <Link
+            href={"/schedule"}
+            className="flex flex-1 cursor-pointer flex-col items-center justify-between gap-4 bg-[#e6ded6] p-3 transition-colors hover:bg-[#e2c8ae] lg:flex-row lg:rounded-br-xl lg:border-2 lg:border-l-[0] lg:border-transparent lg:border-t-transparent lg:bg-transparent lg:hover:border-[#e29579] lg:hover:bg-transparent"
+          >
+            <span className="text-center text-2xl font-bold lg:text-brand">
+              Розклад <br className="sm:hidden" /> Богослужінь
+            </span>
+            <Image
+              className="mx-[0] my-[auto] lg:hidden"
+              width={35}
+              height={35}
+              src={"/svgs/schedule.svg"}
+              alt="calendar"
+            />
+            <Image
+              className="mx-[0] my-[auto] hidden lg:block"
+              width={25}
+              height={25}
+              src={"/svgs/schedule-light.svg"}
+              alt="calendar"
+            />
+          </Link>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </section>
+      <hr className="h-2 w-full border-0 bg-[#a19d99] lg:hidden" />
     </div>
   );
 }
