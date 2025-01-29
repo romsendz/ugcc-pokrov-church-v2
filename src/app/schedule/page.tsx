2fe6b168141b0dbd605ab720a4280cc9e18330ee
romsendz <sendzyuk.r@gmail.com>
2025-01-29 20:53:34 +0100

    schedule page

---
+++ src/app/schedule/page.tsx
@@ -0,0 +1,11 @@
+import Schedule from "@components/Schedule";
+
+const Page = () => {
+  return (
+    <div className="flex h-screen px-4 py-4">
+      <Schedule />
+    </div>
+  );
+};
+
+export default Page;
---
+++ src/components/Schedule/index.tsx
@@ -0,0 +1,10 @@
+const Schedule = () => {
+  return (
+    <iframe
+      className="m-h-[35cm] mx-[auto] my-[0] w-[21cm] rounded-xl [box-shadow:0_0_117px_-6px_grey]"
+      src="https://docs.google.com/document/d/e/2PACX-1vS6lNrqzQW1YrC2-hjrf1-nqj7-B4QJjQn65AYy8IV0Jr3tf1NVoNaIOcQtoUfm1g/pub?embedded=true"
+    />
+  );
+};
+
+export default Schedule;
