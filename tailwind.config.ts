0aa099ed2cea9f5be50d70dca1b0144ad407e805
romsendz <sendzyuk.r@gmail.com>
2025-01-29 20:35:54 +0100

    basic setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,10 @@
 import type { Config } from "tailwindcss";
+import typography from "@tailwindcss/typography";
 
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/layouts/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
@@ -11,8 +13,38 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        header: "#312f2d",
+        footer: "#312f2d",
+        brand: {
+          DEFAULT: "#f5f5dc",
+          dark: "#c2c2a0",
+          light: "#ffffff",
+        },
+      },
+      animation: {
+        heroVideoCoverFadeOutMobile:
+          "heroVideoCoverFadeMobile 37s ease-in-out forwards",
+        heroVideoCoverFadeOutDesktop:
+          "heroVideoCoverFadeOutDesktop 37s ease-in-out forwards",
+      },
+
+      keyframes: {
+        heroVideoCoverFadeMobile: {
+          "0%": { opacity: "1" },
+          "18%": { opacity: "1" },
+          "20%": { opacity: "0" },
+          "98%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+        heroVideoCoverFadeOutDesktop: {
+          "0%": { opacity: "1" },
+          "6%": { opacity: "1" },
+          "7%": { opacity: "0" },
+          "98%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
       },
     },
   },
-  plugins: [],
+  plugins: [typography()],
 } satisfies Config;
