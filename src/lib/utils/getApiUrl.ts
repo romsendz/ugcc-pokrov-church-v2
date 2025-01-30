661bade6ba14ae3ec01fec2408900e1b1fe47dc1
romsendz <sendzyuk.r@gmail.com>
2025-01-30 10:14:51 +0100

    updated usage of env variable

--- src/lib/utils/getApiUrl.ts
+++ src/lib/utils/getApiUrl.ts
@@ -1,4 +1,4 @@
 export function getApiUrl(path: string) {
-  const baseUrl = process.env.PUBLIC_API_URL || "http://localhost:3000";
+  const baseUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
   return `${baseUrl}${path}`;
 }
