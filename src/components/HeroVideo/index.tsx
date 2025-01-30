f5891f2a60fb42c1740535b613800b539fe2b20e
romsendz <sendzyuk.r@gmail.com>
2025-01-30 15:13:58 +0100

    disable fallback image, let YT do its job

--- src/components/HeroVideo/index.tsx
+++ src/components/HeroVideo/index.tsx
@@ -3,7 +3,7 @@
 import { useAppContext } from "@contexts/AppContext/useAppContext";
 import styles from "./index.module.scss";
 import clsx from "clsx";
-import Image from "next/image";
+//import Image from "next/image";
 
 const HeroVideo = () => {
   const { isMenuOpen } = useAppContext();
@@ -12,7 +12,7 @@ const HeroVideo = () => {
     <section
       className={clsx(
         styles.wrapper,
-        "relative pb-[56.25%] lg:brightness-[0.65] lg:filter",
+        "relative pb-[56.25%] brightness-[0.9] lg:brightness-[0.65] lg:filter",
         {
           [styles.lovely_gradient]: isMenuOpen,
         },
@@ -22,16 +22,17 @@ const HeroVideo = () => {
         frameBorder="0"
         referrerPolicy="strict-origin-when-cross-origin"
         loading="lazy"
-        className="absolute left-[0] top-[0] h-full w-full"
+        className="pointer-events-none absolute left-[0] top-[0] h-full w-full"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         src={`https://www.youtube.com/embed/2CI2UvXvkvA?&playlist=2CI2UvXvkvA&loop=1&autoplay=1&rel=0&showinfo=0&iv_load_policy=3&controls=0&mute=1&disablekb=1&widget_referrer=https://www.ugcc-pokrov.te.ua`}
       />
-      <Image
+      {/* enable image if youtube API does not loop video */}
+      {/* <Image
         className="animate-heroVideoCoverFadeOutMobile lg:animate-heroVideoCoverFadeOutDesktop"
         fill
         alt="Фото парафії"
         src="/img/bg-video-fallback.webp"
-      />
+      /> */}
     </section>
   );
 };
