a9b7dec550745caf4529b81005f4821649a6f9c0
romsendz <sendzyuk.r@gmail.com>
2025-01-30 10:59:32 +0100

    fix api call in different envs

--- src/lib/utils/getApiUrl.ts
+++ src/lib/utils/getApiUrl.ts
@@ -1,4 +1,12 @@
 export function getApiUrl(path: string) {
-  const baseUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
+  const isDev = process.env.NODE_ENV === "development";
+  const baseUrl = isDev
+    ? "http://localhost:3000" // Localhost for development
+    : process.env.NEXT_PUBLIC_API_URL; // Production URL from environment variables
+
+  if (!baseUrl) {
+    throw new Error("NEXT_PUBLIC_API_URL is not set in the environment.");
+  }
+
   return `${baseUrl}${path}`;
 }
