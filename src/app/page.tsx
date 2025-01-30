eca1aeb1eab05d16150e6557db25e174fc5c1dc0
romsendz <sendzyuk.r@gmail.com>
2025-01-30 15:14:17 +0100

    decreased text size in quick access

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
             href={"/live-stream"}
             className="flex flex-1 cursor-pointer flex-col items-center justify-between gap-4 bg-[#e6ded6] p-3 transition-colors hover:bg-[#e2c8ae] lg:flex-row lg:rounded-tr-xl lg:border-2 lg:border-l-[0] lg:border-transparent lg:border-b-transparent lg:bg-transparent lg:p-2 lg:hover:border-[#e29579] lg:hover:bg-transparent"
           >
-            <span className="text-center text-2xl font-bold lg:text-base lg:text-brand xl:text-lg">
+            <span className="text-center text-xl font-bold lg:text-base lg:text-brand xl:text-lg">
               Пряма <br className="sm:hidden" /> трансляція
             </span>
             <LiveStreamButton noLink={true} fallbackIconSize={25} />
@@ -26,7 +26,7 @@ export default function Home() {
             href={"/schedule"}
             className="flex flex-1 cursor-pointer flex-col items-center gap-4 bg-[#e6ded6] p-3 transition-colors hover:bg-[#e2c8ae] lg:flex-row lg:rounded-br-xl lg:border-2 lg:border-l-[0] lg:border-transparent lg:border-t-transparent lg:bg-transparent lg:p-2 lg:hover:border-[#e29579] lg:hover:bg-transparent"
           >
-            <span className="text-center text-2xl font-bold lg:text-base lg:text-brand xl:text-lg">
+            <span className="text-center text-xl font-bold lg:text-base lg:text-brand xl:text-lg">
               Розклад <br className="sm:hidden" /> Богослужінь
             </span>
             <Image
